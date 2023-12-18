fe4f061de25131bb4af539995cee77f9d15bab13
Kllill <131448269+Kllill@users.noreply.github.com>
2023-12-19 00:38:32 +0800

    Add files via upload

---
+++ #2083/scripts_#2083.py
@@ -0,0 +1,48 @@
+import sys
+import time
+import uiautomator2
+
+# coding: utf-8
+#
+import uiautomator2 as u2
+
+d = u2.connect('emulator-5554')
+
+d(description="More options").click()
+d.xpath('//android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.RelativeLayout[1]').click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
+d.xpath('//*[@resource-id="net.gsantner.markor_test:id/recycler_view"]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]').click()
+d(description="Back").click()
